12080491731b670d8f0244f4451d9c2f7bb9a7b2
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-04-26 18:44:22 +0530

    store and components
    store is done and make some starting components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css";
-import config from "./config/config";
-
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice.js";
+import {Header, Footer} from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(config);
-  return (
-    <>
-      <div>A blog app in appwrite</div>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .gerCurrUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=" min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+      <Header />
+      <main>
+       TODO : {/* <Outlet /> */}
+      </main>
+      <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading....</div>
   );
 }
 
