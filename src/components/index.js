1f70d67646724c9fb4af31a069baabb16bca31e3
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-05-01 21:20:51 +0530

    PostCard and a little bit Login

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from './Button.jsx';
+import Input from "./Input.jsx";
 
-export { Header, Footer, LogoutBtn, Container, Logo };
+
+export { Header, Footer, LogoutBtn, Container, Logo, Button, Input };
